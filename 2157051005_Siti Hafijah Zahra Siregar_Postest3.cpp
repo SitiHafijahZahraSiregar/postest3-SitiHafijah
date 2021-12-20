c0f79df591be97b2cb5c359e131ddb63169d0782
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2021-12-21 06:51:03 +0700

    Add files via upload

---
+++ 2157051005_Siti Hafijah Zahra Siregar_Postest3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+
+	/*
+	  Nama  : Siti Hafijah Zahra Siregar
+	  NPM   : 2157051005
+	  Kelas : B 
+	*/
+
+	int jumlah_bitcoin ; cout >> jumlah_bitcoin ;
+
+
+    //input//
+
+    cout << "MENENTUKAN TIPE RANGKING DARI JUMLAH BITCOIN : " << endl ; 
+    cout << "Masukkan Jumlah bitcoin : " cin >> jumlah_bitcoin ;
+
+
+    cout << endl << endl ;
+
+
+    //output//
+
+    if (btc<1){
+    	"Shrimp" ;
+    }else if (btc>=10&&btc<10){
+    	"crab" ;
+    }else if (btc>=10&&btc<50){
+    	"Octopus" ;
+    }else if (btc>=50&&btc<100){
+    	"Fish" ;	
+    }else if (btc>=100&&btc<500){
+    	"Dolpin" ;
+    }else if (btc>=500&&btc<1000){
+    	"Shark" ;
+    }else if (btc>=1000&&btc<5000){
+    	"Whale" ;
+    }else (btc>=5000){
+    	"Humpback" ;
+    }
+
+}
